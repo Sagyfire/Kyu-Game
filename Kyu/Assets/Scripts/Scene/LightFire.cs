d893d89c729dfd6473372936b23d2617b30d9f28
Another S. de Rivia <ana.bolumar11@hotmail.es>
2018-04-16 18:56:31 +0200

    Parpadeo de luces por Script.

---
+++ Kyu/Assets/Scripts/Scene/LightFire.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LightFire : MonoBehaviour {
+
+    private float random;
+    private Light _light;
+
+	// Use this for initialization
+	void Start () {
+        _light = GetComponent<Light>();
+        StartCoroutine(Parpadeo());
+}
+	
+	// Update is called once per frame
+	IEnumerator Parpadeo () {
+        while (true)
+        {
+            random = Random.Range(0, 100);
+            if (random < 40)
+            {
+                _light.intensity = 5;
+                yield return new WaitForSeconds(0.8f);
+            }
+            else
+            {
+                _light.intensity = 3;
+                yield return new WaitForSeconds(0.05f);
+            }
+            
+        }
+	}
+}
