a26f6f9643597492fdd3e00680e08412bda0f155
Another S. de Rivia <ana.bolumar11@hotmail.es>
2018-04-23 01:22:27 +0200

    MenuPrincipal
    Scene: Menu

---
+++ Kyu/Assets/Scripts/Scene/OnPointEnter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class OnPointEnter : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+    public Color colorin, colorado;
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        gameObject.GetComponentInChildren<Text>().color = colorado;
+        //gameObject.GetComponentInChildren<Text>().color = Color.white;
+        gameObject.GetComponentInChildren<Text>().fontSize = 60;
+    }
+
+    // Use this for initialization
+    void IPointerEnterHandler.OnPointerEnter (PointerEventData eventData) {
+        //gameObject.GetComponentInChildren<Text>().color = new Color (110, 110, 110, 255);
+        gameObject.GetComponentInChildren<Text>().color = colorin;
+        gameObject.GetComponentInChildren<Text>().fontSize = 65;
+    }
+	/*
+	// Update is called once per frame
+	void IPointExitHandler.OnPointerExit (PointerEventData eventData) {
+        gameObject.GetComponentInChildren<Text>().color = new Color(0, 0, 255, 255);
+    }*/
+}
+
---
+++ Kyu/Assets/Scripts/Scene/TextColor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TextColor : MonoBehaviour, ISelectHandler, IDeselectHandler
+{
+
+    // Use this for initialization
+    void ISelectHandler.OnSelect(BaseEventData eventData)
+    {
+        gameObject.GetComponentInChildren<Text>().color = new Color(0, 255, 255, 255);
+    }
+
+    // Update is called once per frame
+    void IDeselectHandler.OnDeselect(BaseEventData eventData)
+    {
+        gameObject.GetComponentInChildren<Text>().color = new Color(0, 0, 0, 255);
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        gameObject.GetComponentInChildren<Text>().color = new Color(0, 255, 255, 255);
+    }
+}
