610e28d68b771f4c33b98ffbe1d6a16e0917293a
al315581 <al315581@uji.es>
2018-05-05 18:57:24 +0200

    Añadido máquina de estados para el fantasma.
    Va hacia el núcleo pero cuando ve al jugador cerca va hacia él.

--- Kyu/Assets/Scripts/Enemies/GhostScript.cs
+++ Kyu/Assets/Scripts/Enemies/GhostScript.cs
@@ -7,7 +7,12 @@ public class GhostScript : MonoBehaviour {
     Transform player;
     Transform levelCore;
 
+    public float distToPlayer, distToCore;
+    public float maxDistToPly = 15f;
     NavMeshAgent nav;
+    enum state { idle, following, goCore, death };
+
+    state currentState;
 
     void Awake()
     {
@@ -19,12 +24,99 @@ public class GhostScript : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+        currentState = state.goCore;
+        nav.SetDestination(levelCore.position);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        nav.SetDestination(levelCore.position);
 
-		
-	}
+    void Update()
+    {
+
+        distToPlayer = Vector3.Distance(player.position, this.transform.position);
+        distToCore = Vector3.Distance(levelCore.position, this.transform.position);
+        /* if(distToPlayer<= maxDistToPly)
+         {
+             currentState = state.following;
+             nav.enabled = true;
+             nav.SetDestination(player.position);
+
+         }
+         else
+         {
+             nav.enabled = false;
+         }
+         */
+
+        /*
+        if (!(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0))
+        {
+            currentState = state.death;
+        }
+        */
+
+        switch (currentState)
+        {
+            case state.following:
+                /* if (PlayerMovement.isHidden)
+                 {
+                     currentState = state.goHome;
+                     nav.SetDestination(home.position);  //following -> home
+                 }
+                 else
+                 {
+                     if (distToPlayer > maxDistToPly)
+                     {
+                         currentState = state.idle;  //following -> idle
+                         nav.SetDestination(transform.position);
+
+                     }
+                     else nav.SetDestination(player.position);
+                 }
+                 */
+                if (distToPlayer > maxDistToPly)
+                {
+                    currentState = state.goCore; 
+                    nav.SetDestination(levelCore.position);
+
+                }
+                else
+                {
+                    nav.SetDestination(player.position);
+                }
+                
+                break;
+
+            case state.goCore:
+                if (distToPlayer <= maxDistToPly)
+                {
+                    currentState = state.following; //core -> following
+
+                    nav.SetDestination(player.position);
+                }
+                else
+                {
+                    if (distToCore <= 5)
+                    {
+                        print("HAS PERDIDO UNA VIDA");
+                        Destroy(this.gameObject);
+                    }
+                }
+                break;
+
+            case state.idle:
+                if (distToPlayer <= maxDistToPly)
+                {
+                    currentState = state.following; //idle -> following
+
+                    nav.SetDestination(player.position);
+
+                }
+                break;
+
+            case state.death:
+                nav.enabled = false;
+                break;
+        }
+    }
 }
