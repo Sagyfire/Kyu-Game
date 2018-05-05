d67ddce502ac211472b587ba62a0ca1ac01d6e5a
al315581 <al315581@uji.es>
2018-05-05 18:45:09 +0200

    Añadido enemigo con navmesh

---
+++ Kyu/Assets/Scripts/Enemies/GhostScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GhostScript : MonoBehaviour {
+    Transform player;
+    Transform levelCore;
+
+    NavMeshAgent nav;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        levelCore = GameObject.FindGameObjectWithTag("Core").transform;
+        nav = GetComponent<NavMeshAgent>();
+
+    }
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        nav.SetDestination(levelCore.position);
+
+		
+	}
+}
