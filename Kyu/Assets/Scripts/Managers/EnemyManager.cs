8cee2456edda8c37844f3fd46c1b6457938ea7d3
Another S. de Rivia <ana.bolumar11@hotmail.es>
2018-05-16 22:07:50 +0200

    Ataque de estatua, faltan bugs.

---
+++ Kyu/Assets/Scripts/Managers/EnemyManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour {
+    public int setNumberOfRounds = 0;
+    public static int numberOfRounds;
+    public int setR1, setR2, setR3, setR4, setR5 = 0;
+    public static int r1, r2, r3, r4, r5; //the number of enemies for each round
+
+    public static int currentNumberOfEnemies;
+
+    private void Awake()
+    {
+        numberOfRounds = setNumberOfRounds;
+        r1 = setR1;
+        r2 = setR2;
+        r3 = setR3;
+        r4 = setR4;
+        r5 = setR5;
+        currentNumberOfEnemies = 0;
+    }
+    void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Kyu/Assets/Scripts/Statues/RingAttack.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingAttack : MonoBehaviour {
+
+    private bool particlesRunning = false;
+    public ParticleSystem ps, ps2;
+
+    public SphereCollider sphereColl;
+    public float increaseRadius = 0.02f;
+
+    public GameObject prey; //the enemy that enters the zone
+    // Use this for initialization
+    void Start () {
+        ps.Stop();
+        ps2.Stop();
+        prey = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            prey = other.gameObject;
+            startAttack();
+        }
+    }
+
+
+    private void startAttack()
+    {
+        ps.Play();
+        ps2.Play();
+        StartCoroutine(changePS());
+    }
+
+    IEnumerator changePS()
+    {
+        //var shape = ps.shape;
+       
+        //var radius = shape.radius;
+        ParticleSystem.ShapeModule shapeModule = ps.shape;
+        shapeModule.radius = 0.1f;
+        //ps.Play();
+        ParticleSystem.ShapeModule shapeModule2 = ps2.shape;
+        shapeModule2.radius = 0.1f;
+        
+        while (shapeModule.radius <= sphereColl.radius)
+        {
+            
+            shapeModule.radius += increaseRadius;
+            shapeModule2.radius += increaseRadius;
+            //ps.Play();
+            yield return null;
+            //yield return new WaitForSeconds(0.2f);
+        }
+        yield return new WaitForSeconds(0.5f);
+        ps.Stop();
+        KillEnemy();
+        yield return new WaitForSeconds(0.2f);
+        ps2.Stop();
+        yield return null;
+    }
+
+    private void KillEnemy()
+    {
+        Destroy(prey);
+        prey = null;
+    }
+
+}
